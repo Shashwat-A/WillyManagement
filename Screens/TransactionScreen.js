c25a6b5e992c18cee8ea9955d27ee3ca7ee3d9c2
Shashwat-A <sendmail2ruchi@gmail.com>
2021-05-26 14:22:48 +0530

    aded the self filling text inputs

--- Screens/TransactionScreen.js
+++ Screens/TransactionScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet , Image} from 'react-native'
+import {View, Text, TouchableOpacity, StyleSheet , Image, TextInput} from 'react-native'
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner} from 'expo-barcode-scanner'
 
@@ -11,45 +11,92 @@ export default class TransactionScreen extends React.Component {
       hasCameraPermissions: null,
       scanned: false,
       scannedData: '',
-      bnState: 'normal'
+      bnState: 'normal',
+      scannedStudentId: '',
+      scannedBookId: '',
     }
   }
 
-  getCameraPermission = async () => {
+  getCameraPermission = async (id) => {
     const {status} = await Permissions.askAsync(Permissions.CAMERA)
     this.setState({
       hasCameraPermissions: status==='granted',
-      bnState: 'clicked',
+      bnState: id,
       scanned: false
     })
   }
 
   handleBarcodeScan = async(type, data) => {
-    this.setState({
-      scanned:true,
-      scannedData: data,
-      bnState: 'normal'
-    })
+    const {bnState} = this.state
+
+    if(bnState === "Book Id") {
+      this.setState({
+        scanned:true,
+        scannedBookId: type.data,
+        bnState: 'normal'
+      }) 
+    } else if(bnState === "Student Id") {
+        this.setState({
+          scanned:true,
+          scannedStudentId: type.data,
+          bnState: 'normal'
+        }) 
+    }
   }
 
   render() {
     const hasCameraPermissions = this.state.hasCameraPermissions
     const scanned = this.state.scanned
     const bnState = this.state.bnState
+    console.log(scanned)
     
-    if(bnState === 'clicked' && hasCameraPermissions) {
+    if(bnState != 'normal' && hasCameraPermissions) {
       return(
         <BarCodeScanner 
           onBarCodeScanned = {scanned ? undefined : this.handleBarcodeScan}
           style = {StyleSheet.absoluteFillObject}
         />
       )
-    } else if(bnState === 'normal') {
+    } else if(bnState === 'normal' ) {
       return(
-      <View style={style.container}>
+      <View style={styles.container}>
         <View>
             <Image source={require("../assets/booklogo.jpg")} style={{width: 200, height:200}}/>
             <Text style={{textAlign: 'center', fontSize: 30}}>Willy Management</Text>
+        </View>
+
+        <View style={styles.inpView}>
+          <TextInput 
+            style={styles.inputBx}
+            placeholder= "Book Id"
+            value={this.state.scannedBookId}
+          />
+
+          <TouchableOpacity 
+            style={styles.scanBn}
+            onPress={() => {
+              this.getCameraPermission("Book Id")
+            }}
+          >
+            <Text style={styles.bnText}>Scan</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.inpView}>
+        <TextInput
+            style={styles.inputBx}
+            placeholder= "Student Id"
+            value={this.state.scannedStudentId}
+          />
+
+        <TouchableOpacity 
+          style={styles.scanBn}
+          onPress={() => {
+            this.getCameraPermission("Student Id")
+          }}
+        >
+            <Text style={styles.bnText}>Scan</Text>
+        </TouchableOpacity>
         </View>
       </View>
     )
@@ -57,7 +104,7 @@ export default class TransactionScreen extends React.Component {
   }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -66,13 +113,27 @@ const style = StyleSheet.create({
 
   scanBn: {
     backgroundColor: 'yellow',
-    padding: 10,
-    margin: 10,
-    borderWidth: 4,
+    borderLeftWidth: 0,
+    width: 50,
+    borderWidth: 1.5,
   },
 
   bnText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    marginTop: 10,
+    textAlign: 'center'
+  },
+
+  inputBx: {
+    width: 200,
+    height: 40,
+    borderWidth: 1.5,
+    borderRightWidth: 0,
+    fontSize: 20
+  },
+
+  inpView: {
+    flexDirection: 'row',
+    margin: 20,
   }
 })
